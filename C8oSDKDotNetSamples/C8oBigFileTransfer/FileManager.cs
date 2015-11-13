198dc6ff9cd2f5c64f6df7c11012c1201b929925
Nicolas Albert <nicolasa@convertigo.com>
2015-11-13 11:13:25 +0000

    InProgress #4856 - Harmonize C8oSDKs projects name (Android, dotNet, iOS)
      * keep C8oBigFileTransfer in samples

---
+++ C8oSDKDotNetSamples/C8oBigFileTransfer/FileManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace C8oBigFileTransfer
+{
+    public class FileManager
+    {
+        /// <summary>
+        /// Creates a file at the specified path and returns a file stream pointing to it.
+        /// </summary>
+        public Func<String, Stream> CreateFile;
+        /// <summary>
+        /// Returns a file stream pointing to the specified path.
+        /// </summary>
+        public Func<String, Stream> OpenFile;
+
+        public FileManager(Func<String, Stream> CreateFile, Func<String, Stream> OpenFile)
+        {
+            this.CreateFile = CreateFile;
+            this.OpenFile = OpenFile;
+        }
+
+
+    }
+}
