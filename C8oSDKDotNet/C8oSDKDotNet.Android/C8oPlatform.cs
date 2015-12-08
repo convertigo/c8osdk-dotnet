9f790ba7bfc5d618fbc6776faea8cf96d7543bea
Nicolas Albert <nicolasa@convertigo.com>
2015-12-08 10:51:20 +0000

    InProgress #4856 - Harmonize C8oSDKs projects name (Android, dotNet, iOS)
     * allow .Async() after a .Progress
     * add the Sample05 for iOS and WinPhone
     * automatically set the UiDispatcher

---
+++ C8oSDKDotNet/C8oSDKDotNet.Android/C8oPlatform.cs
@@ -0,0 +1,24 @@
+﻿using Android.OS;
+
+namespace Convertigo.SDK
+{
+    public class C8oPlatform
+    {
+        static public void Init()
+        {
+            Handler mainLooperHandler = new Handler(Looper.MainLooper);
+
+            C8o.UiDispatcher = code =>
+            {
+                if (Looper.MyLooper() == Looper.MainLooper)
+                {
+                    code.Invoke();
+                }
+                else {
+                    mainLooperHandler.Post(code);
+                }
+            };
+            C8oFullSyncCbl.Init();
+        }
+    }
+}
