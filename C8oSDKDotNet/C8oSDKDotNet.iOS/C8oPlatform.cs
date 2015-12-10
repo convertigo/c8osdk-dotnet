a988d7f881a82e285f40eeb54d2ca85c44022cdb
Nicolas Albert <nicolasa@convertigo.com>
2015-12-10 10:53:12 +0000

    InProgress #4856 - Harmonize C8oSDKs projects name (Android, dotNet, iOS)
     * adapt the bigFileTransfer Xamarin demo to the new API
     * refactor bigFileTransfer project into the C8OSDKPCL as the !C8oFileTransfer class

--- C8oSDKDotNet/C8oSDKDotNet.iOS/C8oPlatform.cs
+++ C8oSDKDotNet/C8oSDKDotNet.iOS/C8oPlatform.cs
@@ -1,4 +1,5 @@
 ﻿using Foundation;
+using System.IO;
 
 namespace Convertigo.SDK
 {
@@ -7,6 +8,17 @@ namespace Convertigo.SDK
         static public void Init()
         {
             C8o.defaultUiDispatcher = new NSObject().BeginInvokeOnMainThread;
+
+            C8oFileTransfer.fileManager = new C8oFileManager(path =>
+            {
+                FileStream fileStream = File.Create(path);
+                return fileStream;
+            }, path =>
+            {
+                FileStream fileStream = File.Open(path, FileMode.Open, FileAccess.Read);
+                return fileStream;
+            });
+
             C8oFullSyncCbl.Init();
         }
     }
