a8d8b98f63896cbeffad8297030ff958833be8ad
Nicolas Albert <nicolasa@convertigo.com>
2016-01-29 10:54:11 +0000

    InProgress #4921 - Fix replication issues
     * change the SDK version to 2.0.1

--- C8oSDKDotNet/C8oSDKPCL/C8o.cs
+++ C8oSDKDotNet/C8oSDKPCL/C8o.cs
@@ -76,7 +76,7 @@ namespace Convertigo.SDK
 
         public static string GetSdkVersion()
         {
-            return "2.0.0";
+            return "2.0.1";
         }
 
         //*** Attributes ***//
