85c0f0e217db13b807b5009ff21c743b9ed732d4
Nicolas Albert <nicolasa@convertigo.com>
2016-02-12 09:02:57 +0000

    InProgress #4941 - Prepare NUnit testing for the client SDK .NET
     * commit the NUnit project

---
+++ C8oSDKDotNet/C8oSDKNUnitWPF/C8oTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading;
+
+namespace C8oSDKNUnitWPF
+{
+
+    [TestFixture]
+    class C8oTest
+    {
+        [Test]
+        public void Add()
+        {
+           Assert.AreEqual(4, 2+2);
+        }
+
+        [Test]
+        public void Win()
+        {
+            CrossThreadTestRunner runner = new CrossThreadTestRunner();
+            runner.RunInSTA(
+              delegate
+              {
+                  Console.WriteLine(Thread.CurrentThread.GetApartmentState());
+
+                  MainWindow window = new MainWindow();
+                  window.Show();
+
+                  window.Sub();
+              });
+            Console.WriteLine(Thread.CurrentThread.GetApartmentState());
+        }
+    }
+}
