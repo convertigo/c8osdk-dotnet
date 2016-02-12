fcbc7fa8ef368e3b44ba04a3b48fa6aa170ef013
Nicolas Albert <nicolasa@convertigo.com>
2016-02-12 17:02:01 +0000

    InProgress #4941 - Prepare NUnit testing for the client SDK .NET
     * first true tests

--- C8oSDKDotNet/C8oSDKNUnitWPF/C8oTest.cs
+++ C8oSDKDotNet/C8oSDKNUnitWPF/C8oTest.cs
@@ -1,6 +1,10 @@
-﻿using NUnit.Framework;
+﻿using Convertigo.SDK;
+using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.Threading;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace C8oSDKNUnitWPF
 {
@@ -8,27 +12,110 @@ namespace C8oSDKNUnitWPF
     [TestFixture]
     class C8oTest
     {
-        [Test]
-        public void Add()
+        static readonly string HOST = "buildus.twinsoft.fr";
+        static readonly string PROJECT_PATH = "/convertigo/projects/ClientSDKtesting";
+
+        class Stuff
         {
-           Assert.AreEqual(4, 2+2);
+            internal static readonly Stuff C8O = new Stuff(() =>
+            {
+                return new C8o("http://" + HOST + ":28080" + PROJECT_PATH);
+            });
+
+            private Stuff(Func<object> get)
+            {
+                this.get = get;
+            }
+
+            Func<object> get;
+
+            internal Func<object> Get
+            {
+                get { return get; }
+            }
+        }
+
+        IDictionary<Stuff, object> stuffs = new Dictionary<Stuff, object>();
+
+        T Get<T>(Stuff stuff)
+        {
+            lock (stuff)
+            {
+                object res = stuffs.ContainsKey(stuff) ? stuffs[stuff] : null;
+                if (res == null)
+                {
+                    try
+                    {
+                        res = stuff.Get();
+                    }
+                    catch (Exception e)
+                    {
+                        res = e;
+                    }
+                    stuffs[stuff] = res;
+                }
+                if (res is Exception)
+                {
+                    throw (Exception)res;
+                }
+
+                T t = (T)res;
+
+                return t;
+            }
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            // before any tests
         }
 
         [Test]
-        public void Win()
+        public void C8oBadEndpoint()
         {
-            CrossThreadTestRunner runner = new CrossThreadTestRunner();
-            runner.RunInSTA(
-              delegate
-              {
-                  Console.WriteLine(Thread.CurrentThread.GetApartmentState());
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new C8o("http://" + HOST + ":28080");
+            });
+        }
 
-                  MainWindow window = new MainWindow();
-                  window.Show();
+        [Test]
+        public void C8oDefault()
+        {
+            Get<C8o>(Stuff.C8O);
+        }
 
-                  window.Sub();
-              });
-            Console.WriteLine(Thread.CurrentThread.GetApartmentState());
+        [Test]
+        public void C8oDefaultPing()
+        {
+            var c8o = Get<C8o>(Stuff.C8O);
+            var doc = c8o.CallXml(".Ping").Sync();
+            var pong = doc.XPathSelectElement("/document/pong");
+            Assert.NotNull(pong);
+        }
+
+        [Test]
+        public void C8oDefaultPingOneSingleValue()
+        {
+            var c8o = Get<C8o>(Stuff.C8O);
+            var doc = c8o.CallXml(".Ping", "var1", "value one").Sync();
+            var value = doc.XPathSelectElement("/document/pong/var1").Value;
+            Assert.AreEqual("value one", value);
+        }
+
+        [Test]
+        public void C8oDefaultPingTwoSingleValue()
+        {
+            var c8o = Get<C8o>(Stuff.C8O);
+            var doc = c8o.CallXml(".Ping",
+                "var1", "value one",
+                "var2", "value two"
+            ).Sync();
+            var value = doc.XPathSelectElement("/document/pong/var1").Value;
+            Assert.AreEqual("value one", value);
+            value = doc.XPathSelectElement("/document/pong/var2").Value;
+            Assert.AreEqual("value two", value);
         }
     }
 }
