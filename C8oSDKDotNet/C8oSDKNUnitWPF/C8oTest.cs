cfce0fb71ec8ad7a4dce8ef98d70995334f83f78
Nicolas Albert <nicolasa@convertigo.com>
2016-02-15 13:55:17 +0000

    InProgress #4941 - Prepare NUnit testing for the client SDK .NET
     * add a json check type test
     * add session isolation tests

--- C8oSDKDotNet/C8oSDKNUnitWPF/C8oTest.cs
+++ C8oSDKDotNet/C8oSDKNUnitWPF/C8oTest.cs
@@ -1,9 +1,8 @@
 ﻿using Convertigo.SDK;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Threading;
-using System.Xml.Linq;
 using System.Xml.XPath;
 
 namespace C8oSDKNUnitWPF
@@ -17,9 +16,29 @@ namespace C8oSDKNUnitWPF
 
         class Stuff
         {
+            internal static readonly IDictionary<Stuff, object> stuffs = new Dictionary<Stuff, object>();
+
             internal static readonly Stuff C8O = new Stuff(() =>
             {
                 return new C8o("http://" + HOST + ":28080" + PROJECT_PATH);
+            });
+
+            internal static readonly Stuff C8O_BIS = new Stuff(() =>
+            {
+                return new C8o("http://" + HOST + ":28080" + PROJECT_PATH);
+            });
+            
+            internal static readonly Stuff SetGetInSession = new Stuff(() =>
+            {
+                C8o c8o = Get<C8o>(C8O_BIS);
+                var ts = "" + DateTime.Now.Ticks;
+                var doc = c8o.CallXml(".SetInSession", "ts", ts).Sync();
+                var newTs = doc.XPathSelectElement("/document/pong/ts").Value;
+                Assert.AreEqual(ts, newTs);
+                doc = c8o.CallXml(".GetFromSession").Sync();
+                newTs = doc.XPathSelectElement("/document/session/expression").Value;
+                Assert.AreEqual(ts, newTs);
+                return new object();
             });
 
             private Stuff(Func<object> get)
@@ -29,40 +48,38 @@ namespace C8oSDKNUnitWPF
 
             Func<object> get;
 
-            internal Func<object> Get
+            internal static T Get<T>(Stuff stuff)
             {
-                get { return get; }
+                lock (stuff)
+                {
+                    object res = stuffs.ContainsKey(stuff) ? stuffs[stuff] : null;
+                    if (res == null)
+                    {
+                        try
+                        {
+                            res = stuff.get();
+                        }
+                        catch (Exception e)
+                        {
+                            res = e;
+                        }
+                        stuffs[stuff] = res;
+                    }
+                    if (res is Exception)
+                    {
+                        throw (Exception)res;
+                    }
+
+                    T t = (T)res;
+
+                    return t;
+                }
             }
         }
 
-        IDictionary<Stuff, object> stuffs = new Dictionary<Stuff, object>();
-
         T Get<T>(Stuff stuff)
         {
-            lock (stuff)
-            {
-                object res = stuffs.ContainsKey(stuff) ? stuffs[stuff] : null;
-                if (res == null)
-                {
-                    try
-                    {
-                        res = stuff.Get();
-                    }
-                    catch (Exception e)
-                    {
-                        res = e;
-                    }
-                    stuffs[stuff] = res;
-                }
-                if (res is Exception)
-                {
-                    throw (Exception)res;
-                }
-
-                T t = (T)res;
-
-                return t;
-            }
+            return Stuff.Get<T>(stuff);
         }
 
         [SetUp]
@@ -105,7 +122,7 @@ namespace C8oSDKNUnitWPF
         }
 
         [Test]
-        public void C8oDefaultPingTwoSingleValue()
+        public void C8oDefaultPingTwoSingleValues()
         {
             var c8o = Get<C8o>(Stuff.C8O);
             var doc = c8o.CallXml(".Ping",
@@ -116,6 +133,105 @@ namespace C8oSDKNUnitWPF
             Assert.AreEqual("value one", value);
             value = doc.XPathSelectElement("/document/pong/var2").Value;
             Assert.AreEqual("value two", value);
+        }
+
+        [Test]
+        public void C8oDefaultPingTwoSingleValuesOneMulti()
+        {
+            var c8o = Get<C8o>(Stuff.C8O);
+            var doc = c8o.CallXml(".Ping",
+                "var1", "value one",
+                "var2", "value two",
+                "mvar1", new string[] { "mvalue one", "mvalue two", "mvalue three" }
+            ).Sync();
+            object value = doc.XPathSelectElement("/document/pong/var1").Value;
+            Assert.AreEqual("value one", value);
+            value = doc.XPathSelectElement("/document/pong/var2").Value;
+            Assert.AreEqual("value two", value);
+            value = doc.XPathSelectElement("/document/pong/mvar1[1]").Value;
+            Assert.AreEqual("mvalue one", value);
+            value = doc.XPathSelectElement("/document/pong/mvar1[2]").Value;
+            Assert.AreEqual("mvalue two", value);
+            value = doc.XPathSelectElement("/document/pong/mvar1[3]").Value;
+            Assert.AreEqual("mvalue three", value);
+            value = doc.XPathEvaluate("count(/document/pong/mvar1)");
+            Assert.AreEqual(3.0, value);
+        }
+
+        [Test]
+        public void C8oDefaultPingTwoSingleValuesTwoMulti()
+        {
+            var c8o = Get<C8o>(Stuff.C8O);
+            var doc = c8o.CallXml(".Ping",
+                "var1", "value one",
+                "var2", "value two",
+                "mvar1", new string[] { "mvalue one", "mvalue two", "mvalue three" },
+                "mvar2", new string[] { "mvalue2 one" }
+            ).Sync();
+            object value = doc.XPathSelectElement("/document/pong/var1").Value;
+            Assert.AreEqual("value one", value);
+            value = doc.XPathSelectElement("/document/pong/var2").Value;
+            Assert.AreEqual("value two", value);
+            value = doc.XPathSelectElement("/document/pong/mvar1[1]").Value;
+            Assert.AreEqual("mvalue one", value);
+            value = doc.XPathSelectElement("/document/pong/mvar1[2]").Value;
+            Assert.AreEqual("mvalue two", value);
+            value = doc.XPathSelectElement("/document/pong/mvar1[3]").Value;
+            Assert.AreEqual("mvalue three", value);
+            value = doc.XPathEvaluate("count(/document/pong/mvar1)");
+            Assert.AreEqual(3.0, value);
+            value = doc.XPathSelectElement("/document/pong/mvar2[1]").Value;
+            Assert.AreEqual("mvalue2 one", value);
+            value = doc.XPathEvaluate("count(/document/pong/mvar2)");
+            Assert.AreEqual(1.0, value);
+        }
+
+        [Test]
+        public void C8oCheckJsonTypes()
+        {
+            var c8o = Get<C8o>(Stuff.C8O);
+            var json = c8o.CallJson(".JsonTypes",
+                "var1", "value one",
+                "mvar1", new string[] { "mvalue one", "mvalue two", "mvalue three" }
+            ).Sync();
+            object value = json.SelectToken("document.pong.var1").ToString();
+            Assert.AreEqual("value one", value);
+            value = json.SelectToken("document.pong.mvar1[0]").ToString();
+            Assert.AreEqual("mvalue one", value);
+            value = json.SelectToken("document.pong.mvar1[1]").ToString();
+            Assert.AreEqual("mvalue two", value);
+            value = json.SelectToken("document.pong.mvar1[2]").ToString();
+            Assert.AreEqual("mvalue three", value);
+            value = json.SelectToken("document.pong.mvar1").ToObject<JArray>().Count;
+            Assert.AreEqual(3, value);
+            value = json.SelectToken("document.complex.isNull").ToObject<object>();
+            Assert.IsNull(value);
+            value = json.SelectToken("document.complex.isInt3615").ToObject<int>();
+            Assert.AreEqual(3615, value);
+            value = json.SelectToken("document.complex.isStringWhere").ToObject<string>();
+            Assert.AreEqual("where is my string?!", value);
+            value = json.SelectToken("document.complex.isDoublePI").ToObject<double>();
+            Assert.AreEqual(3.141592653589793, value);
+            var isBool = json.SelectToken("document.complex.isBoolTrue").ToObject<bool>();
+            Assert.IsTrue(isBool);
+            value = json.SelectToken("document.complex.ÉlŸz@-node").ToObject<string>();
+            Assert.AreEqual("that's ÉlŸz@", value);
+        }
+        
+        [Test]
+        public void SetGetInSession()
+        {
+            Get<object>(Stuff.SetGetInSession);
+        }
+
+        [Test]
+        public void CheckNoMixSession()
+        {
+            Get<object>(Stuff.SetGetInSession);
+            var c8o = Get<C8o>(Stuff.C8O);
+            var doc = c8o.CallXml(".GetFromSession").Sync();
+            var expression = doc.XPathSelectElement("/document/session/expression");
+            Assert.IsNull(expression);
         }
     }
 }
