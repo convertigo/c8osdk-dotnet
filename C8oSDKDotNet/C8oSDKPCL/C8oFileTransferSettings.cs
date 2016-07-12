eb0f3ed2a5557551b2682c9b0be3a6d3317ddd8d
Charles Grimont <charlesg@convertigo.com>
2016-07-12 13:32:41 +0000

    #4989 In progress
    DONE: Create c8oFileTransferSettings in .NET

---
+++ C8oSDKDotNet/C8oSDKPCL/C8oFileTransferSettings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convertigo.SDK
+{
+    public class C8oFileTransferSettings : C8oFileTransferBase
+    {
+         
+
+        public C8oFileTransferSettings()
+        {
+
+        }
+
+        public C8oFileTransferSettings(C8oFileTransferSettings c8oFileTransferSettings)
+        {
+            Copy(c8oFileTransferSettings);
+        }
+
+        public C8oFileTransferSettings Clone()
+        {
+            return new C8oFileTransferSettings(this);
+        }
+
+
+        public C8oFileTransferSettings SetMaxRunning(int maxRunning)
+        {
+            if(maxRunning <= 0 || maxRunning > 4)
+            {
+                throw new C8oException("maxRunning must be between 1 and 4");
+            }
+            else
+            {
+                this.maxRunning[0] = maxRunning;
+            }
+            return this;
+        }
+    }
+}
