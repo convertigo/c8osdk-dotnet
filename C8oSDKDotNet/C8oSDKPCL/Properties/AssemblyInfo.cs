92854759793c8ab225c766cb8fd18700543cbcc4
Nicolas Albert <nicolasa@convertigo.com>
2016-08-16 08:37:56 +0000

    InProgress #4989 - C8oSDK 2.0.4 harmonization
     * .NET : update some files to 2.0.4

--- C8oSDKDotNet/C8oSDKPCL/Properties/AssemblyInfo.cs
+++ C8oSDKDotNet/C8oSDKPCL/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Resources;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -26,8 +25,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.4.0")]
+[assembly: AssemblyFileVersion("2.0.4.0")]
 
 [assembly: InternalsVisibleTo("C8oSDKDotNet.Android")]
 [assembly: InternalsVisibleTo("C8oSDKDotNet.iOS")]
